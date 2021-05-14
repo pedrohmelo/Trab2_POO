cd00a44872149fb27b95038c1e6e4d83db0c604a
pedro melo <phmc_1995@hotmail.com>
2021-05-14 20:18:21 -0300

    Trabalho 2 POO Clinica Medica

---
+++ src/EntityManager/ConsultaJpaController.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EntityManager;
+
+import EntityManager.exceptions.NonexistentEntityException;
+import POJO.Consulta;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author Pichau
+ */
+public class ConsultaJpaController implements Serializable {
+
+    public ConsultaJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Consulta consulta) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(consulta);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Consulta consulta) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            consulta = em.merge(consulta);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Long id = consulta.getId();
+                if (findConsulta(id) == null) {
+                    throw new NonexistentEntityException("The consulta with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Long id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Consulta consulta;
+            try {
+                consulta = em.getReference(Consulta.class, id);
+                consulta.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The consulta with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(consulta);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Consulta> findConsultaEntities() {
+        return findConsultaEntities(true, -1, -1);
+    }
+
+    public List<Consulta> findConsultaEntities(int maxResults, int firstResult) {
+        return findConsultaEntities(false, maxResults, firstResult);
+    }
+
+    private List<Consulta> findConsultaEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Consulta.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Consulta findConsulta(Long id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Consulta.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getConsultaCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Consulta> rt = cq.from(Consulta.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
